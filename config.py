16cb2376ec94b527d1c0be64353a0aac72479f93
Iaroslav <yaroslav.da@mail.ru>
2024-02-04 22:25:52 +0300

    api connector HH

---
+++ config.py
@@ -0,0 +1,1 @@
+URL_HH = "https://api.hh.ru/vacancies"
---
+++ src/api_connector.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+
+import requests
+
+from config import URL_HH
+
+
+class APIConnector(ABC):
+    """
+    Абстрактный класс для получения вакансий через API
+    """
+
+    @abstractmethod
+    def __init__(self, search_text: str):
+        """
+        Инициализация полей
+        :param search_text:
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_vacancies(self) -> list[dict]:
+        """
+        Получение списка вакансий
+        :return: Список вакансий
+        """
+        raise NotImplementedError
+
+
+class HeadHunterAPIConnector(APIConnector):
+    def __init__(self, search_text: str, area: int):
+        """
+        Инициализация полей API
+        :param search_text:
+        :param area:
+        """
+        self.__params = {
+            'page': 0,
+            'per_page': 100,
+            'text': search_text,
+            'search_field': 'name',
+            'area': area,
+            'only_with_salary': True
+        }
+
+    def get_vacancies(self) -> list[dict]:
+        """
+            Получение списка вакансий API HeadHunter
+            :return: Список вакансий
+            """
+        response = requests.get(URL_HH, params=self.__params)
+        data = response.json()
+        return data['items']
